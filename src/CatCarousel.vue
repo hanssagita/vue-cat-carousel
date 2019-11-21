e6b7678eb4cad67eb9cb4df0775bfd9c18210162
hanssagita <hans.sagita@gmail.com>
2019-11-21 21:30:50 +0700

    fixing es-lint warning up version

--- src/CatCarousel.vue
+++ src/CatCarousel.vue
@@ -1,46 +1,55 @@
 <template>
   <div class="cat-carousel-container">
-      <div class="cat-carousel__prev-navigation" @click="prev">
-        <template>
-          <slot name="prev-navigation">
-            <div class="cat-carousel__default-nav cat-carousel__default-nav--left">
-              <img src="https://i.imgur.com/JkraWHJ.png">
-            </div>
-          </slot>
-        </template>
-      </div>
-      <div
-        ref="carouselContent"
-        class="cat-carousel__content">
-        <div
-          ref="carouselWrapper"
-          :style="wrapperStyles"
-          class="cat-carousel__content__wrapper">
-          <div
-              ref="carouselItem"
-              v-for="(item, index) in items"
-              class="cat-carousel__content__wrapper__item"
-              :key="index">
-            <template>
-              <slot
-                name="item"
-                :data="item"
-                :index="index"
-              />
-            </template>
+    <div
+      class="cat-carousel__prev-navigation"
+      @click="prev"
+    >
+      <template>
+        <slot name="prev-navigation">
+          <div class="cat-carousel__default-nav cat-carousel__default-nav--left">
+            <img src="https://i.imgur.com/JkraWHJ.png">
           </div>
+        </slot>
+      </template>
+    </div>
+    <div
+      ref="carouselContent"
+      class="cat-carousel__content"
+    >
+      <div
+        ref="carouselWrapper"
+        :style="wrapperStyles"
+        class="cat-carousel__content__wrapper"
+      >
+        <div
+          v-for="(item, index) in items"
+          ref="carouselItem"
+          :key="index"
+          class="cat-carousel__content__wrapper__item"
+        >
+          <template>
+            <slot
+              name="item"
+              :data="item"
+              :index="index"
+            />
+          </template>
         </div>
       </div>
-      <div class="cat-carousel__next-navigation" @click="next">
-        <template>
-          <slot name="next-navigation">
-            <div class="cat-carousel__default-nav">
-              <img src="https://i.imgur.com/JkraWHJ.png">
-            </div>
-          </slot>
-        </template>
-      </div>
     </div>
+    <div
+      class="cat-carousel__next-navigation"
+      @click="next"
+    >
+      <template>
+        <slot name="next-navigation">
+          <div class="cat-carousel__default-nav">
+            <img src="https://i.imgur.com/JkraWHJ.png">
+          </div>
+        </slot>
+      </template>
+    </div>
+  </div>
 </template>
 
 <script src="./cat-carousel.js"></script>
