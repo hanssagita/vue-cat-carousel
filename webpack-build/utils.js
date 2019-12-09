1d0a3c38d23646f9784943747ca59a5958e9e258
gilvent <alvarolukmanto@gmail.com>
2019-12-09 22:50:54 +0700

    add raw webpack setup

---
+++ webpack-build/utils.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const path = require('path')
+const config = require('../config')
+
+module.exports = {
+  resolve: function (dir) {
+    return path.join(__dirname, '..', dir)
+  },
+
+  assetsPath: function (_path) {
+    const assetsSubDirectory = 'static'
+    return path.posix.join(assetsSubDirectory, _path)
+  },
+
+  assetsLibPath: function (_path) {
+    return path.posix.join(config.lib.assetsSubDirectory, _path)
+  }
+}
